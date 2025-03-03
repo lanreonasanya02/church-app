ac9419404fcfafa3e73f2ddb29796a1c34606572
Lanre Adeolu <ladeolu2016@gmail.com>
2025-03-03 16:38:34 +0100

    Resumed OneSignl code. Added the service worker path to Setup component and unregistered previous worker

--- src/components/OneSignalSetup.js
+++ src/components/OneSignalSetup.js
@@ -4,10 +4,18 @@ import { useEffect } from "react";
 export default function OneSignalSetup() {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      window.OneSignalDeferred = window.OneSignalDeferred || [];
-      window.OneSignalDeferred.push(async (OneSignal) => {
-        await OneSignal.init({
-          appId: "c27d6f68-3cc7-4add-ab5b-9f0e3fb9889a",
+      import(
+        "https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+      ).then(() => {
+        window.OneSignalDeferred = window.OneSignalDeferred || [];
+        window.OneSignalDeferred.push(async (OneSignal) => {
+          await OneSignal.init({
+            appId: "c27d6f68-3cc7-4add-ab5b-9f0e3fb9889a",
+            notifyButton: { enable: true },
+            serviceWorkerParam: { scope: "/" }, // Ensure correct scope
+            serviceWorkerPath: "/OneSignalSDKWorker.js",
+            serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
+          });
         });
       });
     }
