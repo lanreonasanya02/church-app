3a78b484c69b444d4b4b7652583f732f8fad7ba4
Lanre Adeolu <ladeolu2016@gmail.com>
2025-03-03 16:50:52 +0100

    Resumed OneSignl code. Added the service worker path to Setup component and unregistered previous worker again

--- src/components/OneSignalSetup.js
+++ src/components/OneSignalSetup.js
@@ -3,23 +3,46 @@ import { useEffect } from "react";
 
 export default function OneSignalSetup() {
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      import(
-        "https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
-      ).then(() => {
-        window.OneSignalDeferred = window.OneSignalDeferred || [];
-        window.OneSignalDeferred.push(async (OneSignal) => {
-          await OneSignal.init({
-            appId: "c27d6f68-3cc7-4add-ab5b-9f0e3fb9889a",
-            notifyButton: { enable: true },
-            serviceWorkerParam: { scope: "/" }, // Ensure correct scope
-            serviceWorkerPath: "/OneSignalSDKWorker.js",
-            serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
-          });
+    const script = document.createElement("script");
+    script.src = "https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js";
+    script.defer = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.OneSignalDeferred = window.OneSignalDeferred || [];
+      window.OneSignalDeferred.push(async (OneSignal) => {
+        await OneSignal.init({
+          appId: "c27d6f68-3cc7-4add-ab5b-9f0e3fb9889a",
+          serviceWorkerParam: {
+            scope: "/",
+          },
+          serviceWorkerPath: "/OneSignalSDKWorker.js",
+          serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
         });
       });
-    }
+    };
   }, []);
 
   return null;
 }
+
+// useEffect(() => {
+//   if (typeof window !== "undefined") {
+//     import(
+//       "https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+//     ).then(() => {
+//       window.OneSignalDeferred = window.OneSignalDeferred || [];
+//       window.OneSignalDeferred.push(async (OneSignal) => {
+//         await OneSignal.init({
+//           appId: "c27d6f68-3cc7-4add-ab5b-9f0e3fb9889a",
+//           notifyButton: { enable: true },
+//           serviceWorkerParam: { scope: "/" }, // Ensure correct scope
+//           serviceWorkerPath: "/OneSignalSDKWorker.js",
+//           serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
+//         });
+//       });
+//     });
+//   }
+// }, []);
+
+// return null;
