45d0ce85e9b7cddcc36d803cac43a522938dadc0
Lanre Adeolu <ladeolu2016@gmail.com>
2025-03-05 00:32:34 +0100

    Pusher Beams - Corrected typo in instance id preventing registration of devices

--- src/components/PusherSetup.js
+++ src/components/PusherSetup.js
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import * as PusherPushNotifications from "@pusher/push-notifications-web";
 
-const INSTANCE_ID = process.env.NEXT_PUBLIC_PUSHER_INSTANCE_ID;
+const INSTANCE_ID = process.env.NEXT_PUBLIC_PUSHER_BEAMS_INSTANCE_ID;
 
 export default function PusherSetup() {
   useEffect(() => {
@@ -31,21 +31,3 @@ export default function PusherSetup() {
 
   return null;
 }
-
-// export default function PusherSetup() {
-//   useEffect(() => {
-//     if (typeof window !== "undefined" && INSTANCE_ID) {
-//       const beamsClient = new PusherPushNotifications.Client({
-//         instanceId: INSTANCE_ID,
-//       });
-
-//       beamsClient
-//         .start()
-//         .then(() => beamsClient.addDeviceInterest("all-users"))
-//         .then(() => console.log("Successfully registered and subscribed!"))
-//         .catch(console.error);
-//     }
-//   }, []);
-
-//   return null;
-// }
