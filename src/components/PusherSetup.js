5f0f35d506e36e0cbbc52cd1d83f5ecc1e742c94
Lanre Adeolu <ladeolu2016@gmail.com>
2025-03-05 00:14:32 +0100

    Pusher Beams - unregister registered devices to old service worker (onesignal)

--- src/components/PusherSetup.js
+++ src/components/PusherSetup.js
@@ -7,6 +7,16 @@ const INSTANCE_ID = process.env.NEXT_PUBLIC_PUSHER_INSTANCE_ID;
 export default function PusherSetup() {
   useEffect(() => {
     if (typeof window !== "undefined" && INSTANCE_ID) {
+      // Remove any old OneSignal service workers
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.getRegistrations().then((registrations) => {
+          registrations.forEach((registration) => {
+            registration.unregister();
+          });
+        });
+      }
+
+      // Initialize Pusher Beams
       const beamsClient = new PusherPushNotifications.Client({
         instanceId: INSTANCE_ID,
       });
@@ -21,3 +31,21 @@ export default function PusherSetup() {
 
   return null;
 }
+
+// export default function PusherSetup() {
+//   useEffect(() => {
+//     if (typeof window !== "undefined" && INSTANCE_ID) {
+//       const beamsClient = new PusherPushNotifications.Client({
+//         instanceId: INSTANCE_ID,
+//       });
+
+//       beamsClient
+//         .start()
+//         .then(() => beamsClient.addDeviceInterest("all-users"))
+//         .then(() => console.log("Successfully registered and subscribed!"))
+//         .catch(console.error);
+//     }
+//   }, []);
+
+//   return null;
+// }
