c7476f5fe7effd0e4685e2d4213e63c4d317a4d9
Lanre Adeolu <ladeolu2016@gmail.com>
2025-03-10 17:55:53 +0100

    Imported react-onesignal

--- src/components/OneSignalSetup.js
+++ src/components/OneSignalSetup.js
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect } from "react";
+import OneSignal from "react-onesignal";
 
 const ONESIGNAL_APP_ID = process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID;
 
@@ -7,17 +8,29 @@ export default function OneSignalSetup() {
   useEffect(() => {
     if (typeof window !== "undefined") {
       window.OneSignal = window.OneSignal || [];
-      OneSignal.push(function () {
-        OneSignal.init({
-          appId: ONESIGNAL_APP_ID,
-          allowLocalhostAsSecureOrigin: true,
-          notifyButton: {
-            enable: true,
-          },
-        });
+      OneSignal.init({
+        appId: ONESIGNAL_APP_ID,
+        notifyButton: {
+          enable: true,
+        },
       });
     }
   }, []);
 
   return null;
 }
+
+// useEffect(() => {
+//   if (typeof window !== "undefined") {
+//     window.OneSignal = window.OneSignal || [];
+//     OneSignal.push(function () {
+//       OneSignal.init({
+//         appId: ONESIGNAL_APP_ID,
+//         allowLocalhostAsSecureOrigin: true,
+//         notifyButton: {
+//           enable: true,
+//         },
+//       });
+//     });
+//   }
+// }, []);
