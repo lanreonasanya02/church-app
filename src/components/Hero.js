d8cd13a40f8eb29666c19ea7363dabd80f18464d
Lanre Adeolu <ladeolu2016@gmail.com>
2025-03-20 18:26:10 +0100

    Mobile Queries: Changed animation for live button and also added program to button text again

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ScrambleText from "@/components/ScrambledText";
 import ScheduleModal from "@/components/ScheduleModal";
 import LiveServiceModal from "@/components/LiveServiceModal";
